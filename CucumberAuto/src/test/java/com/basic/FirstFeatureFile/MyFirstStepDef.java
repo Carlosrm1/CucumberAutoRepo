f263a717500e659c2c87411c70ff3b56fb358f65
criojam <crioja@iteggo.com>
2019-01-31 16:26:23 +0100

    second commit

--- CucumberAuto/src/test/java/com/basic/FirstFeatureFile/MyFirstStepDef.java
+++ CucumberAuto/src/test/java/com/basic/FirstFeatureFile/MyFirstStepDef.java
@@ -23,6 +23,7 @@ public class MyFirstStepDef {
 	System.setProperty("webdriver.chrome.driver", "C:\\CucumberSetup\\chromedriver_win32\\chromedriver.exe");	
 	driver =new ChromeDriver();
 	driver.get ("https://www.facebook.com/");
+	System.out.println("open url");
 		
 		
 	}
